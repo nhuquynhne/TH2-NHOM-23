5b6b12151589835ca5e550194786efa2230311bf
nhuquynh <quynhduong0911@gmail.com>
2022-05-14 14:30:23 +0700

    upload project

---
+++ TH2/src/test/java/com/example/TH2/Th2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TH2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Th2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
